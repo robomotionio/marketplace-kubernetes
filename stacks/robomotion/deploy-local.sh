f404150d151a6c655a326ab6e3081135c8ac28d6
Faik Uygur <faik@mosteknoloji.com>
2020-02-18 14:04:43 +0300

    wait migrations

--- stacks/robomotion/deploy-local.sh
+++ stacks/robomotion/deploy-local.sh
@@ -25,3 +25,6 @@ kubectl rollout status -w deployment/robomotion-scheduler
 kubectl rollout status -w deployment/robomotion-authorization
 kubectl rollout status -w deployment/robomotion-admin
 kubectl rollout status -w deployment/robomotion-designer
+
+# ensure migrations completed
+kubectl wait --for=condition=complete --timeout=300s job/robomotion-migrations
